156e1c1caccc93be4a013fce72970134ce4d300e
nahom zenebe <nahomzenebe123@gmail.com>
2024-12-03 23:24:36 +0300

    sixth commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,12 +7,14 @@ import SignupPage from "./pages/SignupPage";
 import ProfilePage from "./pages/ProfilePage";
 import { Routes,Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
+import {useThemeStore} from './store/useThemeStore'
 import {Loader} from 'lucide-react'
 import { Toaster } from "react-hot-toast";
 function App() {
 
   const {authUser,checkAuth,isCheckingAuth}=useAuthStore()
-
+  
+ const{theme}=useThemeStore()
 
 
   useEffect(()=>{
@@ -36,13 +38,13 @@ function App() {
 
 
   return (
-    <div >
+    <div data-theme={theme}>
       <Navbar/>
     <Routes>
     <Route index element={ authUser ? <HomePage/>:<Navigate to='/login'/>} />
     <Route path='/signup' element={!authUser?<SignupPage/>:<Navigate to='/'/>} />
     <Route path='/login' element={!authUser? <LoginPage/>:<Navigate to='/'/>} />
-    <Route path='/setting' element={<SettingPage/>} />
+    <Route path='/settings' element={<SettingPage/>} />
     <Route path='/profile' element={authUser ?<ProfilePage/>:<Navigate to='/login'/>}/>
 
 </Routes>
--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,10 +1,9 @@
-import {create} from 'zustand'
+import { create } from "zustand";
 
-
-export const useAuthStore=create((set)=>({
-  theme:localStorage.getItem("chat-theme")||"light",
-  setTheme:(theme)=>{
-    localStorage.setItem("chat-theme",theme)
-    set({theme})
-  }
-}))
+export const useThemeStore = create((set) => ({
+  theme: localStorage.getItem("chat-theme") || "coffee",
+  setTheme: (theme) => {
+    localStorage.setItem("chat-theme", theme);
+    set({ theme });
+  },
+}));
